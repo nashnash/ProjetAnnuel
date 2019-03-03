bd8b4ef85aa980d81923654486e2f040196286a9
nashad <nashadhedoo@gmail.com>
2019-03-03 01:39:59 +0100

    V5
    Ajout du bouton valider + connexion BDD lorsque l'ont clique sur "valider"

--- Project C/NoWast/main.c
+++ Project C/NoWast/main.c
@@ -4,19 +4,26 @@
 #include <MYSQL/mysql.h>
 #include "connectSQL.h"
 
+
+void on_activate_entry(GtkWidget *pEntry, gpointer data);
+void on_copier_button(GtkWidget *pButton, gpointer data);
+
 int main(int argc, char **argv)
 {
 MYSQL mysql;
         mysql_init(&mysql);
         mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"option");
-connectdb();
+
     GtkWidget *pWindow;
     GtkWidget *pVBox;
     GtkWidget *pFrame;
     GtkWidget *pVBoxFrame;
-
+GtkWidget *pButton;
     GtkWidget *pEntry;
     GtkWidget *pLabel;
+
+
+
     gchar *sUtf8;
 
     gtk_init(&argc, &argv);
@@ -39,6 +46,8 @@ connectdb();
     pVBoxFrame = gtk_vbox_new(TRUE, 0);
     gtk_container_add(GTK_CONTAINER(pFrame), pVBoxFrame);
 
+
+
     /* Creation et insertion des elements contenus dans le premier GtkFrame */
     pLabel = gtk_label_new("Entrez votre nom d'utilisateur :");
     gtk_box_pack_start(GTK_BOX(pVBoxFrame), pLabel, TRUE, FALSE, 0);
@@ -52,9 +61,20 @@ connectdb();
     pEntry = gtk_entry_new();
     gtk_box_pack_start(GTK_BOX(pVBoxFrame), pEntry, TRUE, FALSE, 0);
 
+ pButton = gtk_button_new_with_label("Valider");
+    gtk_box_pack_start(GTK_BOX(pVBox), pButton, TRUE, FALSE, 0);
+
+     pLabel = gtk_label_new(NULL);
+    gtk_box_pack_start(GTK_BOX(pVBox), pLabel, TRUE, FALSE, 0);
 
 
+/* Connexion du signal "activate" du GtkEntry */
+    g_signal_connect(G_OBJECT(pEntry), "activate", G_CALLBACK(on_activate_entry), (GtkWidget*) pLabel);
 
+
+    /* Connexion du signal "clicked" du GtkButton */
+    /* La donnee supplementaire est la GtkVBox pVBox */
+    g_signal_connect(G_OBJECT(pButton), "clicked", G_CALLBACK(on_copier_button), (GtkWidget*) pVBox);
 
 
     gtk_widget_show_all(pWindow);
@@ -62,4 +82,25 @@ connectdb();
     gtk_main();
 
     return EXIT_SUCCESS;
+}
+
+
+/* Fonction callback execute lors du signal "activate" */
+void on_activate_entry(GtkWidget *pEntry, gpointer data)
+{
+    const gchar *sText;
+
+    /* Recuperation du texte contenu dans le GtkEntry */
+    sText = gtk_entry_get_text(GTK_ENTRY(pEntry));
+
+    /* Modification du texte contenu dans le GtkLabel */
+    gtk_label_set_text(GTK_LABEL((GtkWidget*)data), sText);
+    connectdb();
+}
+
+/* Fonction callback executee lors du signal "clicked" */
+void on_copier_button(GtkWidget *pButton, gpointer data)
+{
+
+    connectdb();
 }
