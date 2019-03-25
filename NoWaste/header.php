9c0cd0cde1bb496b27feded9de818423ec6f020e
nashad <nashadhedoo@gmail.com>
2019-03-25 01:16:53 +0100

    Ajout du site Web

---
+++ NoWaste/header.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html  lang="fr-FR">
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="NoWaste" />
+
+    <!-- Stylesheets
+    ============================================= -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="css/swiper.css" type="text/css" />
+    <link rel="stylesheet" href="css/dark.css" type="text/css" />
+    <link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+    <link rel="stylesheet" href="css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Document Title
+    ============================================= -->
+    <title>FightFoodWaste | Ensemble évitons le gaspillage</title>
+
+    <style>
+        #top-header-buttons {
+            float: right;
+            margin: 33px 0;
+            -webkit-transition: margin .4s ease;
+            -o-transition: margin .4s ease;
+            transition: margin .4s ease;
+        }
+
+        #header.sticky-header #top-header-buttons { margin: 13px 0; }
+
+        #top-header-buttons a { margin: 0 0 0 15px; }
+
+        #top-header-buttons a:not(.button) { color: #FFF; }
+        #top-header-buttons a:not(.button):hover { color: #DDD; }
+
+        #header.sticky-header #top-header-buttons a:not(.button) { color: #1ABC9C; }
+        #header.sticky-header #top-header-buttons a:not(.button):hover { color: #111; }
+
+        .device-md #top-header-buttons {
+            position: absolute;
+            top: 0;
+            left: auto;
+            right: 15px;
+        }
+    </style>
+
+</head>
+
+<header id="header" class="transparent-header full-header" data-sticky-class="not-dark" style="background-color: #7a0012">
+
+    <div id="">
+
+        <div class="container clearfix">
+
+            <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+            <!-- Logo
+            ============================================= -->
+            <div id="logo">
+                <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>
+                <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+            </div><!-- #logo end -->
+
+            <!-- Primary Navigation
+            ============================================= -->
+            <nav id="primary-menu" class="dark">
+
+                <ul>
+                    <li class="current"><a href="index.html"><div>Accueil</div></a>
+
+                    <li><a href="#"><div>Qui sommes-nous ?</div></a>
+                        <ul>
+                            <li><a href="#"><div><i class="icon-stack"></i>Concept</div></a>
+
+                            </li>
+
+
+                            <li><a href="contact.html"><div><i class="icon-envelope-alt"></i>Présentation de l'équipe</div></a>
+
+                            </li>
+
+                        </ul>
+                    </li>
+
+                    <li><a href="#"><div>Support / FAQ</div></a>
+                        <ul>
+                            <li><a href="#"><div><i class="icon-stack"></i>Question / Réponse</div></a>
+
+                            </li>
+
+
+                            <li><a href="contact.html"><div><i class="icon-envelope-alt"></i>Contactez service client</div></a>
+
+                            </li>
+
+                        </ul>
+                    </li>
+
+
+
+                    <li class="mega-menu"><a href="#"><div>Contactez-nous</div></a>
+
+                    </li>
+
+
+
+                </ul>
+
+                <div id="top-header-buttons" class="d-none d-md-block">
+                    <a href="#" class="button button-3d button-small button-rounded">Inscrivez-vous</a>
+                    <a href="#">Se connecter<i class="icon-caret-right" style="position: relative; top: 2px; margin-left:3px;"></i></a>
+                </div>
+
+            </nav><!-- #primary-menu end -->
+
+        </div>
+
+    </div>
+
+</header><!-- #header end -->
